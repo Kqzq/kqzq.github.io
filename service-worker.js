7d7d64cfb1c76cd8d06e79fec53e70c36a27ec73
Kqzq <66525670+Kqzq@users.noreply.github.com>
2025-03-06 14:06:32 +0100

    Transformation PWA

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", event => {
+    console.log("Service Worker installé !");
+    event.waitUntil(
+        caches.open("greentrack-cache").then(cache => {
+            return cache.addAll([
+                "index.html",
+                "app.js",
+                "styles.css",
+                "icons/icon-192x192.png",
+                "icons/icon-512x512.png"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
